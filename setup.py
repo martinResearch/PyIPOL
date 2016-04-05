71969e78da47822307a427f3e41a340266528e44
martinGithub <martindlg@yahoo.fr>
2016-04-05 17:11:32 +0200

    tryin gto get pip to work

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup,Extension
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from distutils.extension import Extension
 from Cython.Build import cythonize
+
 import numpy as np
 
 # Get the version number.
 import runpy
 __version_str__ = runpy.run_path("ipol/version.py")["__version_str__"]
 
-__version_str__ = runpy.run_path("ipol/download_ipol.py")
+runpy.run_path("ipol/download_ipol.py")
 
 sources = ['ipol/_ipol.pyx','ipol/csources/lsd_1.6/lsd.c','ipol/csources/classic_edge_detectors_1.0/classic_edge_detectors.c']
 extensions = Extension('ipol._ipol',sources, extra_compile_args=['-std=c99'])               
