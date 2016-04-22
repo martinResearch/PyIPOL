43d0af9930e7f9c0ce57bc1755991e87575a25bd
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2016-04-22 21:05:09 +0200

    remove calls to "sudo apt-get ..." on */install.py

--- ipol/wrappers/Rudin_Osher_Fatemi_Total_Variation_Denoising_using_Split_Bregman/install.py
+++ ipol/wrappers/Rudin_Osher_Fatemi_Total_Variation_Denoising_using_Split_Bregman/install.py
@@ -13,7 +13,6 @@ def _install():
    """this function downloads and compile the code for the chanvese implementation"""
    download_file='http://www.ipol.im/pub/art/2012/g-tvd/tvdenoise_20120516.tar.gz'
    tools.download_and_extract(download_file)  
-   os.system( 'sudo apt-get install build-essential libjpeg8-dev libpng-dev libtiff-dev')
    this_file_path=os.path.dirname(__file__)
    subprocess.call('make -f makefile.gcc', shell=True,cwd=exec_folder)   
    
