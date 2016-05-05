c937f03eb52c22a734704b5c4be3e0201cc4bcf2
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2016-05-05 10:58:31 +0200

    [ACE] adapt to python 3

--- ipol/wrappers/__init__.py
+++ ipol/wrappers/__init__.py
@@ -8,7 +8,7 @@ def run_all_examples():
 	
 	for filename in _list_wrappers_paths():
 		f=os.path.join(filename,'examples.py')
-		print 'running %s...'%f
+		print('running %s...'%f)
 
 		execfile(f,{'__name__' : '__main__'})
 		print('done')
