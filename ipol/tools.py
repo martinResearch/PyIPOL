38e4e1fb58d1d1488b0a2f9d10ab648c60ec687c
martinResearch <martin.delagorce@gmail.com>
2018-06-24 21:30:54 +0200

    trying to fix travis osx
    trying to fix travis osx

--- ipol/tools.py
+++ ipol/tools.py
@@ -84,7 +84,7 @@ def _list_wrappers_paths(path=None):
 	
 	
 def _install(filename):
-	
+		print('installing %s'%filename)
 		if os.path.isfile(os.path.join(filename,'setup.py')):#cython binding
 			os.system('cd %s;python setup.py build_ext --inplace'%filename)
 			#execfile(' %s/setup.py '%filename,[build_ext --inplace])
