0145f4cf9b832ff0376fc79821f9dcbbdc83a27a
martinResearch <martin.delagorce@gmail.com>
2018-06-30 00:33:40 +0200

    fixing some windows compilations

--- ipol/wrappers/Automatic_Color_Enhancement_and_its_Fast_Implementation/install.py
+++ ipol/wrappers/Automatic_Color_Enhancement_and_its_Fast_Implementation/install.py
@@ -1,20 +1,28 @@
 import os
 import tools
 import subprocess
-
-
+import platform
+from shutil import copy
 string="""Automatic Color Enhancement (ACE) and its Fast Implementation
 Pascal Getreuer"""
 
 
 
-exec_folder=tools.extraction_directory+'/ace_20121029'
+exec_folder=os.path.join(tools.extraction_directory,'ace_20121029')
 source_directory=exec_folder
 def _install():
-   """this function downloads and compile the code for the chanvese implementation"""
-   download_file='http://www.ipol.im/pub/art/2012/g-ace/ace_20121029.tar.gz'
-   tools.download_and_extract(download_file) 
-   subprocess.call('make -f makefile.gcc', shell=True,cwd=exec_folder)   
-   
-   
+        """this function downloads and compile the code for the chanvese implementation"""
+        download_file='http://www.ipol.im/pub/art/2012/g-ace/ace_20121029.tar.gz'
+        tools.download_and_extract(download_file) 
+        if platform.system()=="Windows":
+                tools.download_and_extract('ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll32.zip') 
+                path=os.path.dirname(__file__)
+                #replacing the makefile.gcc file
+                copy(os.path.join(path,'patch','makefile.gcc'), os.path.join(exec_folder,'makefile.gcc'))
+                subprocess.call('make CC=gcc -f makefile.gcc', shell=True,cwd=exec_folder)  
+                
+        else:
+                subprocess.call('make -f makefile.gcc', shell=True,cwd=exec_folder)   
+
+
 
--- ipol/wrappers/chanvese_segmentation/install.py
+++ ipol/wrappers/chanvese_segmentation/install.py
@@ -18,5 +18,15 @@ def _install():
    download_file='http://www.ipol.im/pub/art/2012/g-cv/chanvese_20120715.tar.gz'
    tools.download_and_extract(download_file)  
    this_file_path=os.path.dirname(__file__)
-   subprocess.call('make -f makefile.gcc', shell=True,cwd=exec_folder)   
+ 
    
+   if platform.system()=="Windows":
+      
+      path=os.path.dirname(__file__)
+      #replacing the makefile.gcc file
+      copy(os.path.join(path,'patch','makefile.gcc'), os.path.join(exec_folder,'makefile.gcc'))
+      copy(os.path.join(path,'patch','imageio.c'), os.path.join(exec_folder,'makefile.c'))
+      subprocess.call('make CC=gcc -f makefile.gcc', shell=True,cwd=exec_folder)  
+
+   else:
+      subprocess.call('make -f makefile.gcc', shell=True,cwd=exec_folder)   
