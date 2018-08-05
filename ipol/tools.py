15b4a2eeae8e6b78679d14ba1c2c19f749c25d8f
martinResearch <martin.delagorce@gmail.com>
2018-08-05 23:03:14 +0200

    Update tools.py

--- ipol/tools.py
+++ ipol/tools.py
@@ -85,6 +85,7 @@ def _list_wrappers_paths(path=None):
 def _install(filename):
 		print('installing %s'%filename)
 		if os.path.isfile(os.path.join(filename,'setup.py')):#cython binding
+			pass
 			subprocess.call('python setup.py build_ext --inplace',shell=True,cwd=filename)
 			#os.system('cd %s;python setup.py build_ext --inplace'%filename)
 			#execfile(' %s/setup.py '%filename,[build_ext --inplace])
